1de2da130dfc00c61560ca6e9ec155a52c7cb98a
Auliviet <112747495+auliviet@users.noreply.github.com>
2025-10-06 21:08:45 +1100

    feat(component-library): create the ProgressBar component

--- CSS for JavaScript/mini-component-library/src/components/ProgressBar/ProgressBar.js
+++ CSS for JavaScript/mini-component-library/src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,79 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const SIZES = {
+  small: {
+    height: 8,
+    radius: 4,
+    padding: 0,
+  },
+  medium: {
+    height: 12,
+    radius: 4,
+    padding: 0,
+  },
+  large: {
+    height: 24,
+    radius: 8,
+    padding: 4,
+  },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const MIN = 0;
+  const MAX = 100;
+  const styles = SIZES[size];
+
+  console.log(styles);
+
+  if (value < MIN) {
+    value = MIN;
+  } else if (value > MAX) {
+    value = MAX;
+  }
+  return (
+    <Component
+      role="progressbar"
+      style={{
+        "--padding": styles.padding + "px",
+        "--radius": styles.radius + "px",
+      }}
+      aria-valuenow={value}
+    >
+      <ProgressWrapper>
+        <Progress
+          style={{
+            "--progress": value + "%",
+            "--height": styles.height + "px",
+          }}
+        ></Progress>
+      </ProgressWrapper>
+    </Component>
+  );
 };
+
+const Component = styled.div`
+  padding: var(--padding);
+  border-radius: var(--radius);
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const ProgressWrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+const Progress = styled.div`
+  background-color: ${COLORS.primary};
+  width: var(--progress);
+  height: var(--height);
+`;
 
 export default ProgressBar;
