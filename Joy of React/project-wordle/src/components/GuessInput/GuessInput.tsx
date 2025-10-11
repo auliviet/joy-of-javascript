ef1d6fd4dff009c4374a115968629b56acea77f9
Auliviet <112747495+auliviet@users.noreply.github.com>
2025-10-12 06:59:46 +1100

    feat(wordle): create GuessInput component

---
+++ Joy of React/project-wordle/src/components/GuessInput/GuessInput.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function GuessInput() {
+  const [guess, setGuess] = useState("");
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    setGuess("");
+    console.log(guess);
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        id="guess-input"
+        type="text"
+        pattern="[a-zA-Z]{5}"
+        maxLength={5}
+        value={guess}
+        onChange={(event) => {
+          setGuess(event.target.value.toUpperCase());
+        }}
+      />
+    </form>
+  );
+}
